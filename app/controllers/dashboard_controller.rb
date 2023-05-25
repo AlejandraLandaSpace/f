d8a167c2c8d8e4bb7a2e7c5841807938efadef5f
Alejandra <aleriquene@gmail.com>
2023-05-25 12:06:36 -0500

    added pagination

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,8 @@ class DashboardController < ApplicationController
   before_action(:force_user_sign_in)
 
   def index
-    # @posts = Post.all.order({ :created_at => :desc })
     @q = Post.ransack(params[:q])
-    @posts = @q.result
+    @posts = @q.result.page params[:page]
     render({ :template => "dashboard/index.html.erb" })
   end
 end
