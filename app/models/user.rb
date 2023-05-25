5bcfc849aa023f7e7c04d90ff31e86a3c6f7c93c
Alejandra <aleriquene@gmail.com>
2023-05-24 20:09:07 -0500

    fixed if not current user edit options

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,5 @@ class User < ApplicationRecord
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
   has_secure_password
+  has_many :posts
 end
